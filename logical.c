d00b66ac15af493eadb2cbb068505e63a9b874ea
dinnu-25 <92150161+dinnu-25@users.noreply.github.com>
2021-10-09 14:35:41 +0530

    Add files via upload

---
+++ logical.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	int a=20,b=10;
+	printf("%d\n",a<b);
+	printf("%d\n",a<=b);
+	printf("%d\n",a>b);
+	printf("%d\n",a>=b);
+	printf("%d\n",a==b);
+	printf("%d",a!=b);
+	
+	
+}
