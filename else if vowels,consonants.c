5e1f2c2992a75b14a38f321404e5a2c4cdced2ae
dinnu-25 <92150161+dinnu-25@users.noreply.github.com>
2021-10-11 14:27:35 +0530

    Add files via upload

---
+++ else if vowels,consonants.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+int main()
+{
+	char ch;
+	scanf("%c",&ch);
+	if(ch=='a')
+	{
+		printf("vowel");
+	}
+		else if(ch=='e')
+		{
+			printf("vowel");
+		}
+		else if(ch=='i')
+		{
+			printf("vowel");
+		}
+		else if(ch=='o')
+		{
+			printf("vowel");
+		}
+		else if(ch=='u')
+		{
+			printf("vowel");
+		}
+		else
+		{
+			printf("consonants");
+		}
+		
+	}
+		
+			
+		
+	
+
