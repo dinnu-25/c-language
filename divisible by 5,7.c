4765271430f2a8137805b10cb8baec7cc0d10e03
dinnu-25 <92150161+dinnu-25@users.noreply.github.com>
2021-10-12 13:58:50 +0530

    Add files via upload

---
+++ divisible by 5,7.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	int a;
+	scanf("%d",&a);
+	printf("%d",a);
+	if(a%5==0 && a%7==0)
+	{
+		printf("divisible by 5 and 7");
+	}
+	else
+	{
+		printf("not divisible");
+	}
+}
